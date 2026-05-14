208ba1b2ea7390050be91105fd2414462ae4bffe
yuu-kc8 <yuu-kc8@users.noreply.github.com>
2026-05-14 11:51:50 +0900

    Day74 空入力メッセージを変更

--- code-practice/day01_shopping_mini/script.js
+++ code-practice/day01_shopping_mini/script.js
@@ -3,7 +3,7 @@ function showPlan() {
   const result = document.getElementById("result");
   
   if (food.trim() === "") {
-  result.innerHTML = "<p>食材を入力してください。</p>";
+  result.innerHTML = `<p>先に使う食材を入力してください。</p>`;
   return;
 }
 
